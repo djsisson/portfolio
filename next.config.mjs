9859e7388efc6a205a222f95d6eadcce311580d2
Darren Sisson <darrensisson@hotmail.com>
2024-06-29 18:03:28 +0100

    make standalone

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  experimental: {
+    reactCompiler: "true",
+  },
+};
 
 export default nextConfig;
