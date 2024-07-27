d6de93d98373162d7a799e96204c0ad73147823b
Darren Sisson <darrensisson@hotmail.com>
2024-07-27 14:18:47 +0100

    fix delay

--- src/app/asteroidz/components/VisualScore.tsx
+++ src/app/asteroidz/components/VisualScore.tsx
@@ -19,41 +19,19 @@ const VisualScore = ({
     return () => clearTimeout(timeout);
   }, [x, remove]);
 
-  const position = () => {
-    switch (type) {
-      case "Click": {
-        return "left-[calc(15%_+_var(--rndLeft))]";
-      }
-      case "Cps": {
-        return "right-[calc(15%_+_var(--rndLeft))]";
-      }
-    }
-  };
-
-  const animation = () => {
-    switch (x.crit) {
-      case true: {
-        return "animation-name: crit;";
-      }
-      case false: {
-        return "animation-name: up;";
-      }
-    }
-  };
-
   if (!x.id) return null;
 
   return (
     <div
-      className={`absolute top-[calc(40%_+_var(--rndTop))] hidden inline-block text-[var(--bgcolour)] opacity-0 ${position()}`}
+      className={`absolute hidden inline-block text-[var(--bgcolour)] opacity-0`}
       style={
         {
-          "--rndLeft": `${x.rndLeft}px`,
-          "--rndTop": `${x.rndTop}px`,
-          "--delay": `${x.delay}ms`,
+          top: `calc(40% + ${x.rndTop}px)`,
+          left: type == "Click" ? `calc(15% + ${x.rndLeft}px)` : undefined,
+          right: type == "Cps" ? `calc(15% + ${x.rndLeft}px)` : undefined,
           animationTimingFunction: "cubic-bezier(0.23, 1, 0.32, 1)",
           animationFillMode: "forwards",
-          animationDelay: "var(--delay)",
+          animationDelay: `${x.delay}ms`,
           animationDuration: "3s",
           animationIterationCount: "1",
           transformOrigin: "center",
