f9d877e8eea3008ac6ad2c8f1b8f9138cab77e30
Darren Sisson <darrensisson@hotmail.com>
2024-08-05 19:23:53 +0100

    fix bug

--- src/app/auth/private/page.tsx
+++ src/app/auth/private/page.tsx
@@ -2,13 +2,13 @@ import { redirect } from "next/navigation";
 import { getUserFromJWT, signOut } from "@/utils/auth";
 
 export default async function Page() {
-  const user = await getUserFromJWT();
+  const user: any = await getUserFromJWT();
   if (!user) {
     redirect("/auth/");
   }
   return (
     <div>
-      {user.email!}
+      {user?.email || ""}
       <form>
         <button formAction={signOut}>Logout</button>
       </form>
