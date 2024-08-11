4f46200ab41cd7b0b1f3ffa66a3231bc1a9e6c25
Darren Sisson <darrensisson@hotmail.com>
2024-08-11 16:57:34 +0100

    fix date hydration error

--- src/app/todos/components/Todo.tsx
+++ src/app/todos/components/Todo.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { CircleCheck, CircleX, Info } from "lucide-react";
+import { CircleCheck, CircleX } from "lucide-react";
 import { deleteTodo, toggleTodo } from "../actions";
 import {
   Tooltip,
@@ -9,12 +9,19 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { todos } from "@/db/todos/schema";
+import { useState, useEffect } from "react";
 export default function Todo({ todo }: { todo: typeof todos.$inferSelect }) {
+  const [currentDate, setCurrentDate] = useState("");
+  const [currentTime, setCurrentTime] = useState("");
+  useEffect(() => {
+    setCurrentDate(todo.createdAt.toLocaleDateString());
+    setCurrentTime(todo.createdAt.toLocaleTimeString());
+  }, [todo.createdAt]);
   return (
     <div className="flex flex-col justify-between gap-2 rounded-lg border-1 p-2">
       <div className="text-wrap break-all select-none">{todo.title}</div>
       <div className="flex items-center justify-between">
-        <div className="text-xs italic">{`${todo.createdAt.toLocaleDateString()} ${todo.createdAt.toLocaleTimeString()}`}</div>
+        <div className="text-xs italic">{`${currentDate} ${currentTime}`}</div>
         <div className="flex justify-end gap-1">
           <TooltipProvider>
             <Tooltip>
